39dfd49ce5803304d9dc916657d413731eb960e9
Chiara <chiaraborgatti.96@gmail.com>
2022-10-22 01:21:42 -0300

    Initial commit

---
+++ Assets/MenuPausa.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public GameObject menuDePausa;
+    private bool menuOn;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            menuOn = !menuOn;
+        }
+        if(menuOn==true){
+            menuDePausa.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+        else{
+            menuDesactivado();
+        }
+    }
+    public void Continuar(){
+        menuDesactivado();
+        menuOn = false; 
+    }
+    public void Salir(){
+        Application.Quit();
+    }
+    private void menuDesactivado(){
+        menuDePausa.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Hols # Alice
+ 
