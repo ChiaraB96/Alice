44acf9f059c11aee1c51bb8683e6467dc4e93bb3
Nehuen P <nehuenpeyrano@gmail.com>
2022-10-31 13:41:25 -0300

    Personaje
    Personaje con camara, media chueca.

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotationSpeed = 90;
+    public float gravity = -20;
+    public float jumpSpeed = 15;
+
+    CharacterController characterController;
+    Vector3 moveVelocity;
+    Vector3 turnVelocity;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        if(characterController.isGrounded){
+            moveVelocity = transform.forward * speed * vInput;
+            turnVelocity = transform.up * rotationSpeed * hInput;
+            if(Input.GetButtonDown("Jump")){
+                moveVelocity.y = jumpSpeed;
+            }
+        }
+        moveVelocity.y +=gravity * Time.deltaTime;
+        characterController.Move(moveVelocity * Time.deltaTime);
+        transform.Rotate(turnVelocity * Time.deltaTime);
+    }
+}
