20adbbb1fcfb654ae8d36c3eae5215cb58860742
Nehuen P <nehuenpeyrano@gmail.com>
2022-12-12 15:38:22 -0300

    Objetos inventario

---
+++ Assets/New Folder/InventoryItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class InventoryItem
+
+{
+    public InventoryItemData data { get; private set; }
+    public int stackSize { get; private set;}
+
+    public InventoryItem(InventoryItemData source)
+    {
+        data = source;
+        AddToStack;
+    }
+    public void AddToStack()
+    {
+        atackSize++;
+    }
+    public void RemoveFromStack()
+    {
+        stackSize--;
+    }
+}
