d8b52e927e7f0e9ce31eded6f7922378b06cffde
ChiaraB96 <chiaraborgatti.96@gmail.com>
2022-12-13 19:11:11 -0300

    Torreta todavia no anda bien

---
+++ Assets/Torreta/Disparo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public GameObject bala;
+    public float tiempo = 2f;
+    private int contador;
+    public int maxCont = 20;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Disparar());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Disparar(){
+        Debug.Log("Iniciar corrutina");
+        for(int i=0; i<maxCont; i++) {
+            contador++;
+            Instantiate(bala, transform.position, transform.rotation);
+            yield return new WaitForSeconds (tiempo);
+        }
+        Debug.Log("Fin corutina");
+    }
+}
+
