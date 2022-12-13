43d1168eb4333abf6d2ea2b588634add9e697669
Nehuen P <nehuenpeyrano@gmail.com>
2022-12-13 12:15:44 -0300

    CasiAgarrarObjetosInventario

---
+++ Assets/Objetos e inventario/ItemObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public InventoryItemData referenceItem;
+
+    public void OnHandlePickUpItem()
+    {
+        InventorySystemm.Instance.Add(referenceItem);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            OnHandlePickUpItem();
+        }
+    }
+}
