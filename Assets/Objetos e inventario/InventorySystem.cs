0a4e7443295a6d4bcb62a460de71d06ccc151221
Nehuen P <nehuenpeyrano@gmail.com>
2022-12-15 02:29:26 -0300

    Inventario?

--- Assets/Objetos e inventario/InventorySystem.cs
+++ Assets/Objetos e inventario/InventorySystem.cs
@@ -6,6 +6,9 @@ public class InventorySystem : MonoBehaviour
 {
     public static InventorySystem Instance;
 
+    public delegate void onInventoryChangedEvent();
+    public event onInventoryChangedEvent onInventoryChangedCallBack;
+
     private Dictionary<InventoryItemData, InventoryItem> _itemDictionary;
     public List<InventoryItem> inventory;
 
@@ -16,26 +19,30 @@ public class InventorySystem : MonoBehaviour
 
         Instance = this;
     }
-    /*public InventoryItem Get(InventoryItemData itemData)
+    public InventoryItem Get(InventoryItemData itemData)
     {
         if(_itemDictionary.TryGetValue(itemData, out InventoryItem value))
         {
             return value;
         }
         return null;
-    }*/
+    }
 
     public void Add(InventoryItemData itemData)
         {
             if(_itemDictionary.TryGetValue(itemData, out InventoryItem value))
             {
                 value.AddToStack();
+
+                onInventoryChangedCallBack.Invoke();
             }
             else
             {
                 InventoryItem newItem = new InventoryItem(itemData);
                 inventory.Add(newItem);
                 _itemDictionary.Add(itemData, newItem);
+
+                onInventoryChangedCallBack.Invoke();
             }
         }
         public void Remove(InventoryItemData itemData)
@@ -50,5 +57,7 @@ public class InventorySystem : MonoBehaviour
                     _itemDictionary.Remove(itemData);
                 }
             }
+
+            onInventoryChangedCallBack.Invoke();
         }
 }
