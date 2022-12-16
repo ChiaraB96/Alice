781faf2ac8ffa4251777032fff57ddff46b838e0
ChiaraB96 <chiaraborgatti.96@gmail.com>
2022-12-16 11:27:39 -0300

    nose

---
+++ Assets/Puertas/Puerta.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour, IInteractable
+{
+    public void Interact(PlayerMovement player);
+    {
+        
+    }
+}
