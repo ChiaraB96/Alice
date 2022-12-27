ee6b963751e2a66d3a8f148181627c61d774cbc2
ChiaraB96 <chiaraborgatti.96@gmail.com>
2022-12-27 18:26:14 -0300

    puerta q no anda

---
+++ Assets/Puertas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puertas : MonoBehaviour
+{
+    public GameObject door; // Asigna la puerta en el Inspector de Unity
+    public GameObject key; // Asigna la llave en el Inspector de Unity
+
+    private bool hasKey; // Almacena si el jugador tiene la llave o no
+
+    void OnTriggerEnter(Collider other)
+    {
+        CheckKey(); // Verifica si el jugador tiene la llave
+    }
+
+    void CheckKey()
+    {
+        if (hasKey)
+        {
+            OpenDoor(); // Abre la puerta
+        }
+        else
+        {
+            Debug.Log("No tienes la llave necesaria para abrir esta puerta"); // Muestra un mensaje al jugador
+        }
+    }
+
+    void OpenDoor()
+    {
+                door.GetComponent<BoxCollider>().enabled = false; // Desactiva el collider de la puerta
+        door.transform.Rotate(0, 90, 0); // Rota la puerta 90 grados en el eje Y
+    }
+}
