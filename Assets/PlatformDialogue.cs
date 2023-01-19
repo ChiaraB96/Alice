e0083925968410ea5118fe056ee15db632b0a187
Nehuen P <nehuenpeyrano@gmail.com>
2023-01-19 11:33:38 -0300

    Te odio Haskell

--- Assets/PlatformDialogue.cs
+++ Assets/PlatformDialogue.cs
@@ -10,6 +10,7 @@ public class PlatformDialogue : MonoBehaviour
     private Dictionary<string, string> platformDialogues; // Diccionario para almacenar los dialogos segun la plataforma
     private Dictionary<string, bool> platformDialoguesShown; // Diccionario para registrar si el dialogo de cada plataforma ya se ha mostrado
     private Coroutine currentCoroutine; // variable para guardar la corutina actual
+    private bool dialogueComplete = false; // bandera para indicar si el dialogo se ha completado
 
     private void Start()
     {
@@ -33,21 +34,25 @@ public class PlatformDialogue : MonoBehaviour
             {
                 if (!platformDialoguesShown[gameObject.name]) // si el dialogo de esta plataforma aun no se ha mostrado
                 {
+                    dialogueComplete = false;
                     currentCoroutine = StartCoroutine(ShowText(platformDialogues[gameObject.name])); // se inicia la corutina y se pasa como parametro el dialogo correspondiente a la plataforma actual
                     platformDialoguesShown[gameObject.name] = true; // se registra que el dialogo de esta plataforma ya se ha mostrado
                 }
             }
         }
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player")) // si el objeto con el que colisiona tiene el tag "Player"
         {
-            if(currentCoroutine != null)
-                StopCoroutine(currentCoroutine); // detiene la corutina actual en ejecucion 
-            dialogueBox.SetActive(false); //se desactiva el cuadro de dialogo
-            dialogueText.text = ""; // se limpia el texto
+            if(dialogueComplete)
+            {
+                if(currentCoroutine != null)
+                    StopCoroutine(currentCoroutine); // detiene la corutina actual en ejecucion 
+                dialogueBox.SetActive(false); //se desactiva el cuadro de dialogo
+                dialogueText.text = ""; // se limpia el texto
+                dialogueComplete = false;
+            }
         }
     }
     IEnumerator ShowText(string text) // se define la corutina
@@ -59,5 +64,6 @@ public class PlatformDialogue : MonoBehaviour
             dialogueText.text += c; // se añade la letra actual al texto
             yield return new WaitForSeconds(0.05f); // espera 0.05 segundos antes de mostrar la siguiente letra
         }
+        dialogueComplete = true;
     }
 }
