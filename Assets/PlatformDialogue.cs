2201627d745cbdec114d5af0c90f79fa1def7159
Nehuen P <nehuenpeyrano@gmail.com>
2023-01-23 11:47:41 -0300

    Dialogo

--- Assets/PlatformDialogue.cs
+++ Assets/PlatformDialogue.cs
@@ -9,11 +9,13 @@ public class PlatformDialogue : MonoBehaviour
     public Text dialogueText; // Es el texto donde se muestra el dialogo 
     private Dictionary<string, string> platformDialogues; // Diccionario para almacenar los dialogos segun la plataforma
     private Dictionary<string, bool> platformDialoguesShown; // Diccionario para registrar si el dialogo de cada plataforma ya se ha mostrado
+    private bool isShowing; // variable para registrar si se esta mostrando el dialogo
+    private Coroutine showTextCoroutine; // referencia a la coroutine ShowText
 
     private void Start()
     {
         platformDialogues = new Dictionary<string, string>();
-        platformDialogues.Add("Platform1", "¡Bienvenido a esta plataforma!");
+        platformDialogues.Add("Platform1", "¡Bienvenido a esta plataformalalalalalalalalalalalala!");
         platformDialogues.Add("Platform2", "¡Bienvenido a esta plataforma2!");
         platformDialogues.Add("Platform3", "¡Bienvenido a esta plataforma3!");
         //agregar mas plataformas y sus dialogos 
@@ -22,18 +24,20 @@ public class PlatformDialogue : MonoBehaviour
         {
             platformDialoguesShown.Add(platform, false); // se agrega una entrada para cada plataforma con el valor de falso
         }
+        isShowing = false;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player")) // si el objeto con el que colisiona tiene el tag "Player"
+        if (other.gameObject.CompareTag("Player") && !isShowing) // si el objeto con el que colisiona tiene el tag "Player" y no se esta mostrando el dialogo
         {
             if (platformDialogues.ContainsKey(gameObject.name)) // si el diccionario contiene una entrada con el nombre de la plataforma actual
             {
                 if (!platformDialoguesShown[gameObject.name]) // si el dialogo de esta plataforma aun no se ha mostrado
                 {
-                    StartCoroutine(ShowText(platformDialogues[gameObject.name])); // se inicia la corutina y se pasa como parametro el dialogo correspondiente a la plataforma actual
+                    showTextCoroutine = StartCoroutine(ShowText(platformDialogues[gameObject.name])); // se guarda la referencia a la coroutine y se inicia la corutina y se pasa como parametro el dialogo correspondiente a la plataforma actual
                     platformDialoguesShown[gameObject.name] = true; // se registra que el dialogo de esta plataforma ya se ha mostrado
+                    isShowing = true; // se registra que se esta mostrando el dialogo
                 }
             }
         }
@@ -41,22 +45,24 @@ public class PlatformDialogue : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) // si el objeto con el que colisiona tiene el tag "Player"
+        if (other.gameObject.CompareTag("Player")) // si el objeto con el que colisiona tiene el tag "Player"
         {
-            StopAllCoroutines(); // detiene cualquier coroutine en ejecucion 
+            StopCoroutine(showTextCoroutine); // detiene la coroutine ShowText
             dialogueBox.SetActive(false); //se desactiva el cuadro de dialogo
             dialogueText.text = ""; // se limpia el texto
+	        isShowing = false; // se registra que ya no se esta mostrando el dialogo
         }
     }
     
     IEnumerator ShowText(string text) // se define la corutina
     {
-        dialogueBox.SetActive(true); // se activa el cuadro de dialogo
-        dialogueText.text = ""; // se limpia el texto 
+        dialogueText.text = ""; //se limpia el texto
+        dialogueBox.SetActive(true); // se activa el cuadro de dialogo 
         foreach (char c in text) // se recorre cada letra del dialogo
         {
             dialogueText.text += c; // se añade la letra actual al texto
-            yield return new WaitForSeconds(0.3f); // espera 0.05 segundos antes de mostrar la siguiente letra
+            yield return new WaitForSeconds(0.05f); // espera 0.3 segundos antes de mostrar la siguiente letra
         }
+        isShowing = false; // se registra que ya no se esta mostrando el dialogo
     }
 }
