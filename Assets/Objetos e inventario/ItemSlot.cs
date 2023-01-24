28e62a055245c9eb98022c07457fb0551c2dbc40
Nehuen P <nehuenpeyrano@gmail.com>
2023-01-24 15:45:10 -0300

    Particulas y puertas Tutorial
    La mujeres ya no lloran las mujeres facturan.

--- Assets/Objetos e inventario/ItemSlot.cs
+++ Assets/Objetos e inventario/ItemSlot.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
-    [SerializeField]
-    private Image _itemIcon;
 
-    [SerializeField]
-    private GameObject _stackObj;
+    public Image _itemIcon;
 
-    [SerializeField]
-    private TextMeshProUGUI _stackNumber;
+    public GameObject _stackObj;
+
+    public TextMeshProUGUI _stackNumber;
 
     public void Set(InventoryItem item)
     {
         _itemIcon.sprite = item.data.itemIcon;
 
-        if (item.stackSize <= 0)
+        if (item.stackSize <= 1)
         {
             _stackObj.SetActive(false);
             return;
