3432fbf5d462c7fa2ca0f93df22dc670217f9f4c
Nehuen P <nehuenpeyrano@gmail.com>
2023-02-02 11:00:52 -0300

    Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void EscenaJuego(){
-        SceneManager.LoadScene("Tuto");
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void Salir(){
