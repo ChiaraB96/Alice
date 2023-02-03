4a1f58a45dc381a241f4fe2870a29ed05912c72d
ChiaraB96 <chiaraborgatti.96@gmail.com>
2023-02-03 15:51:19 -0300

    arregle cositas tengo miedo

--- Assets/abreMenuPausa.cs
+++ Assets/abreMenuPausa.cs
@@ -12,7 +12,7 @@ public class abreMenuPausa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("p")){
             menuOn = true;
         }
         if(menuOn==true){
