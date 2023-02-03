618768f941aa9a8f4df9ea6ff6f22fcdc15155f5
ChiaraB96 <chiaraborgatti.96@gmail.com>
2023-02-03 13:02:59 -0300

    Boton ahora anda falta terminar de acomodar

--- Assets/MenuPausa.cs
+++ Assets/MenuPausa.cs
@@ -5,29 +5,22 @@ using UnityEngine;
 public class MenuPausa : MonoBehaviour
 {
     public GameObject menuDePausa;
-    private bool menuOn;
-
+    public GameObject confirmar;
 
     // Update is called once per frame
-    void Update()
+
+    public void Continuar () 
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            menuOn = !menuOn;
-        }
-        if(menuOn==true){
-            menuDePausa.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            Time.timeScale = 0;
-        }
-        else{
-            menuDesactivado();
-        }
-    }
-    private void menuDesactivado(){
-        menuDePausa.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
+       menuDePausa.SetActive(false);
+        //Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Time.timeScale = 1;
     }
+
+    public void Salir() 
+    {
+        confirmar.SetActive(true);
+    }
+
+   
 }
---
+++ Assets/abreMenuPausa.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abreMenuPausa : MonoBehaviour
+{
+    public GameObject menuDePausa;
+    private bool menuOn = false;
+    public GameObject confirmar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            menuOn = !menuOn;
+        }
+        if(menuOn==true){
+            menuDePausa.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+        else{
+            if(menuDePausa.activeSelf) {
+                DesactivarMenu();
+            }
+        }
+    }
+    
+     public void Continuar() 
+    {
+       DesactivarMenu();
+    }
+
+    public void Salir() 
+    {
+        confirmar.SetActive(true);
+    }
+
+    private void DesactivarMenu(){
+        menuDePausa.SetActive(false);
+       Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+}
