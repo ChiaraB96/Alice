f54b043c9a2655eb8011248159709ca67ef1f032
ChiaraB96 <chiaraborgatti.96@gmail.com>
2023-05-31 18:41:45 -0300

    Sistema Respawn Mejorado

--- Assets/Scripts/Nuevo sistema Respawn/Respawn.cs
+++ Assets/Scripts/Nuevo sistema Respawn/Respawn.cs
@@ -19,6 +19,9 @@ public class Respawn : MonoBehaviour {
     public Transform punto4;
     public Transform punto5;
     public Transform punto6;
+    public Transform punto7;
+    public Transform punto8;
+    public Transform punto9;
     public Transform final;
 
     private UltimaPlataforma ultimaPlataforma; //ultima plataforma que tocó el jugador
@@ -46,6 +49,15 @@ public class Respawn : MonoBehaviour {
                 break;
             case "R6":
                 transform.position = punto6.position;
+                break;
+            case "R7":
+                transform.position = punto7.position;
+                break;
+            case "R8":
+                transform.position = punto8.position;
+                break;
+            case "R9":
+                transform.position = punto9.position;
                 break;
             case "RFinal":
                 transform.position = final.position;
